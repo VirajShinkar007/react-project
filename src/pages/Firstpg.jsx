379b8790c025d6a2e0032844b95ab34e9676d727
VirajShinkar007 <virajdshinkar@gmail.com>
2025-05-27 16:56:50 +0530

    make changes

--- src/pages/Firstpg.jsx
+++ src/pages/Firstpg.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Firstpg.css';
 import { Link } from 'react-router-dom';
+import popx from "../assets/popx.png";
+
 
 
 const Firstpg = () => {
   return (
+    
+
+  
+
+
     <div className='container'>
+
+      <div className='super'>
+        <img id='ram' src={popx} alt="" />
+      </div>
         <h1 className='h1mess'>
             Welcome to PopX
         </h1>
